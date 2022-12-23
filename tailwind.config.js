30e4d646e90fd7057316e5066b9acbc6331bbef2
daatoo <davit.cheishvili.06@gmail.com>
2022-12-23 16:12:34 +0400

    Add Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
 content: ["./src/**/*.{html,js}"],
 theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'LightBlue': '#31C3BD',
+        'LightBlueHover': '#65E9E4',
+        'LightYellow': '#F2B137',
+        'LightYellowHover': '#FFC860',
+        'DarkNavy': '#1A2A33',
+        'SemiDarkNavy': '#1F3641',
+        'Silver': '#A8BFC9',
+        'SilverHover': '#DBE8ED',
+      }
+    },
   },
   plugins: [],
 }
